79108217868681775d71d2427f8416f25e38c68a
outaTiME <afalduto@gmail.com>
2013-08-17 16:26:34 -0300

    Fixes.

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.use(connect.basicAuth(function (user, pass) {
 }));
 */
 
-app.use(connect.static(__dirname + '/public'));
+app.use(connect.static(__dirname + "/vendor"));
+// app.use("/static", connect.static(__dirname + "/vendor/geolocation-track"));
+// app.use("/static", express.static(__dirname + "/alternate_static"));
+app.use(connect.directory(__dirname + "/vendor"));
+
+// app.use(connect.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3001, function () {
   console.log("Listening on port %d", this.address().port);
